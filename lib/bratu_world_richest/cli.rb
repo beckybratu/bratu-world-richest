ba5fc13bf100d053a3a4dcb5d69c26462f087d3c
Rebecca Bratu <beckybratu@gmail.com>
2017-09-24 01:55:18 +0000

    Starting to build the scraper that will grab information on the billionaires from Forbes.

--- lib/bratu_world_richest/cli.rb
+++ lib/bratu_world_richest/cli.rb
@@ -9,20 +9,23 @@ class BratuWorldRichest::CLI
   end
 
   def list_richest
+    puts "These are the three richest people in the world today:"
     @richest = BratuWorldRichest::Richest.now
+    @richest.each.with_index(1) do |rich_person, i|
+      puts "#{i}. #{rich_person.name} - Net worth: #{rich_person.networth} - Source of wealth: #{rich_person.source}"
+    end
   end
 
   def menu
     input = nil
     while input != "exit"
-      puts "Enter the number of the billionaire you'd like to get to know or type 'list' to see the Top 5:"
+      puts "Enter the number of the billionaire you'd like to get to know or type 'list' to see the Top 3:"
       input = gets.strip.downcase
-      case input
-      when "1"
-        puts "Here's how number 1 made a fortune..."
-      when "2"
-        puts "Here's how number 2 got rich..."
-      when "list"
+
+      if input.to_i > 0
+        the_richest = @richest[input.to_i - 1]
+        puts "#{the_richest.name} - Net worth: #{the_richest.networth} - Source of wealth: #{the_richest.source}"
+      elsif input == "list"
         list_richest
       else
         puts "You look lost. Type 'list' or 'exit'."
@@ -31,7 +34,7 @@ class BratuWorldRichest::CLI
   end
 
   def goodbye
-    puts "Thanks for visiting! Go make some money!"
+    puts "Thanks for visiting! Go make some money so you can be on this list someday!"
   end
 
 end
--- lib/bratu_world_richest/richest.rb
+++ lib/bratu_world_richest/richest.rb
@@ -1,17 +1,28 @@
 class BratuWorldRichest::Richest
-  attr_accessor :name, :networth, :url
+  attr_accessor :name, :networth, :source, :url
 
   def self.now
     #returns instances of current wealthiest people
+    self.scrape_forbes
+  end
+
+  def self.scrape_forbes
+
+    three_richest =[]
+
     richest_1 = self.new
     richest_1.name = "Buffet"
     richest_1.networth = "75 billion"
+    richest_1.source = "Berkshire Hathaway"
     richest_1.url = "https://www.forbes.com/profile/warren-buffett/?list=billionaires"
 
     richest_2 = self.new
     richest_2.name = "Gates"
     richest_2.networth = "86 billion"
+    richest_2.source = "Microsoft"
     richest_2.url = "https://www.forbes.com/profile/bill-gates/?list=billionaires"
+
+    three_richest
   end
 
 end
